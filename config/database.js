bf861f6b80fa1610bf18e446e68bddf1704802d2
Murilo S. Rodrigues Jr <murilo.cc.uepb@gmail.com>
2021-01-14 23:05:41 -0300

    modify database file

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const bdConfig = require('./config.json');
-const connection;
+var connection
 if (process.env.NODE_ENV==="prod") connection = new Sequelize(bdConfig.production);
 if (process.env.NODE_ENV==="test") connection = new Sequelize(bdConfig.test);
 if (!process.env.NODE_ENV) connection = new Sequelize(bdConfig.development);
